4c3b72d68f48ab46c22414516c2e9b92055e06d6
Jason Marsell <jasonmarsell@myblueorbit.com>
2020-02-07 15:21:47 -0800

    Completed authentication and JWT additions to API.

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,10 +1,16 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Threading.Tasks;
+using System.Security.Claims;
+using System.Text;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 using DatingApp.API.Data;
-using DatingApp.API.Models;
 using DatingApp.API.DTOs;
+using DatingApp.API.Models;
 
 namespace DatingApp.API.Controllers
 {
@@ -13,10 +19,12 @@ namespace DatingApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
 
-        public AuthController(IAuthRepository repo)
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
             this._repo = repo;
+            this._config = config;
         }
 
         [HttpGet]
@@ -30,10 +38,8 @@ namespace DatingApp.API.Controllers
         [Route("Register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
-            // Validate the request
-
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
-            if(await this._repo.UserExists(userForRegisterDto.Username))
+            if (await this._repo.UserExists(userForRegisterDto.Username))
                 return BadRequest("Username already exists");
 
             var userToCreate = new User()
@@ -44,6 +50,39 @@ namespace DatingApp.API.Controllers
             var createdUser = await this._repo.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            // Build the claims collection for our JWT
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            // And, create the key and signature
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            // Start building the return JWT token
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new { token = tokenHandler.WriteToken(token) });
         }
     }
 }

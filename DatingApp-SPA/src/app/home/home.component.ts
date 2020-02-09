f39fa20a9a9d935ee4e7265e64bca83b15382d2e
Jason Marsell <jasonmarsell@myblueorbit.com>
2020-02-09 12:04:22 -0800

    Finished 3rd Party additions

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -16,4 +16,8 @@ export class HomeComponent implements OnInit {
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
+  }
 }

790eebee6d88d133827a6053535a2bd984d300fb
Jason Marsell <jasonmarsell@myblueorbit.com>
2020-02-09 08:32:18 -0800

    Finished section 4 auth and register

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -6,7 +6,7 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthService {
-  baseUrl = "http://localhost:5000/api/auth/";
+  baseUrl = 'http://localhost:5000/api/auth/';
   constructor(private http: HttpClient) { }
 
   login(model: any) {
@@ -18,6 +18,10 @@ export class AuthService {
             localStorage.setItem('token', user.token);
           }
         }));
+  }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model);
   }
 
 }
